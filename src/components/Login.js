4474b5b397cda038042bdc72e4e3ef5d106ca9c3
AdamF06 <qq2841864141@gmail.com>
2019-10-20 08:44:01 +1100

    delete public source

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    
+    render() { 
+        return ( 
+            <div className="login">
+
+            </div>           
+         );
+    }
+}
+ 
+export default Login;
---
+++ src/components/SideBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SideBar extends Component {
+    
+    render() { 
+        return ( 
+            <div className="SideBar">
+                
+            </div>
+         );
+    }
+}
+ 
+export default SideBar;
