09881cdf4842894fc6794193a2c2c2f2082c0644
AdamF06 <qq2841864141@gmail.com>
2019-11-10 20:19:08 +1100

    FIX:nev

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SideBar from './components/SideBar/SideBar';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-// import Login from './components/Login/Login';
+import Login from './components/Login/Login';
 import './App.css'
 // import User from './components/User/User'
 import UserRouter from './components/User/UserRouter';
@@ -9,10 +9,10 @@ import UserRouter from './components/User/UserRouter';
 function App() {
   return (
     <>
-    <SideBar/>
+    {/* <SideBar/> */}
     {/* <TopBar/> */}
-    {/* <Login/> */}
-    <UserRouter/>
+    <Login/>
+    {/* <UserRouter/> */}
     </>
   );
 }
