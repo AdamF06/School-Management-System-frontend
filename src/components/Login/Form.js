ac0ea361ba6a8e1a5744a311f1ec9d0f9c24fd20
AdamF06 <qq2841864141@gmail.com>
2019-11-10 22:46:44 +1100

    FIX:CORS request header

--- src/components/Login/Form.js
+++ src/components/Login/Form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import './Form.css'
 import {
     Gmail,
@@ -9,26 +10,45 @@ import {
 const form = document.getElementById("container")
 
 class Form extends Component {
-    state={
-        panel:"left"
+    state = {
+        panel: "left",
+        email:'',
+        password:''
     }
-    switchToSignin(){
+
+    switchToSignin() {
         this.setState({
-            panel:"right"
+            panel: "right"
         })
     }
-    switchToSignup(){
+    switchToSignup() {
         this.setState({
-            panel:"left"
+            panel: "left"
         })
-        
+
+    }
+    signIn = (e) => {
+        e.preventDefault()
+        axios({
+            url: 'http://127.0.0.1:8080/students/login',
+            method: 'post',
+            data: {
+                email: "test@qq.com",
+                password: "1234567"
+            }
+        }).then(res => {
+            console.log(res.data)
+            if (res.status === 200) {
+                console.log("success")
+            }
+        })
     }
 
     render() {
-        const {panel} = this.state
+        const { panel } = this.state
         return (
-            <div className={`login__form ${panel === "left"? "":"right-panel-active"}`} >
-                
+            <div className={`login__form ${panel === "left" ? "" : "right-panel-active"}`} >
+
                 <div className="login__form__signup formContainer signupContainer">
                     <form action="#">
                         <h1>Create Account</h1>
@@ -57,7 +77,7 @@ class Form extends Component {
                         <input type="email" placeholder="Email" />
                         <input type="password" placeholder="Password" />
                         <a href="#">Forgot your password?</a>
-                        <button className="act">Sign In</button>
+                        <button className="act" onClick={this.signIn}>Sign In</button>
                     </form>
                 </div>
                 <div className="login__form__overlayContainer">
@@ -66,7 +86,7 @@ class Form extends Component {
                             <h1>Welcome Back!</h1>
                             <p>To keep connected with us please login with your personal info</p>
                             <button className="act act__ghost" id="signIn"
-                            onClick={(e)=>{this.switchToSignup()}}>
+                                onClick={(e) => { this.switchToSignup() }}>
                                 Sign In
                             </button>
                         </div>
@@ -74,7 +94,7 @@ class Form extends Component {
                             <h1>Hello!</h1>
                             <p>Enter your personal details and start journey with us</p>
                             <button className="act act__ghost" id="signUp"
-                            onClick={(e)=>{this.switchToSignin()}}
+                                onClick={(e) => { this.switchToSignin() }}
                             >Sign Up
                             </button>
                         </div>
