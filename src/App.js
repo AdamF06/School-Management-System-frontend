8a8e77ab42b97402113a2cdfcd88d92664779e33
Hao_Wang <haowang9263@gmail.com>
2019-11-12 18:49:59 +1100

    FEAT-003: Coure router fixed and all components for Course fixed

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
     <SideBar/>
     {/* <TopBar/> */}
     {/* <Login/> */}
-    {/* <UserRouter/> */}
-    <CourseRouter/>
+    <UserRouter/>
+    <CourseRouter />
     </>
   );
 }
--- src/components/Course/CourseRouter.js
+++ src/components/Course/CourseRouter.js
@@ -4,43 +4,38 @@ import {
     Switch,
     Route,
 } from "react-router-dom";
-import CourseMenu from "./CourseMenu";
-import CourseInfo from "./CourseInfo";
-import CourseOutline from "./CourseOutline";
-import Assignment from "./Assignment";
-import Assessment from "./Assessment"
-import PlagiarismPolicy from "./PlagiarismPolicy";
-import FAQ from "./FAQ"
+import CourseNav from "./CourseNav/CourseNav";
+import CourseInfo from "./CourseInfo/CourseInfo";
+import CourseOutline from "./CourseOutline/CourseOutline";
+import Assignment from "./Assignment/Assignment";
+import Assessment from "./Assessment/Assessment"
+import PlagiarismPolicy from "./PlagiarismPolicy/PlagiarismPolicy";
+import FAQ from "./FAQ/FAQ"
 
 export default class CourseRouter extends Component {
     render() {
         return (
             <Router>
-                <CourseMenu />
+                <CourseNav />
                 <Switch>
-                    <Route path="/courseinfo">
-                        <Switch>
-                            <Route exact path="/courseinfo">
-                                <CourseInfo />                                
-                            </Route>
-                            <Route exact path="/courseoutline">
-                                <CourseOutline />                                                                
-                            </Route>
-                            <Route exact path="/assignment">
-                                <Assignment />                                
-                            </Route>
-                            <Route exact path="/assessment">
-                                <Assessment />                                                                
-                            </Route>
-                            <Route exact path="/plagiarismpolicy">
-                                <PlagiarismPolicy />                                                                
-                            </Route>
-                            <Route exact path="/FAQ">
-                                <FAQ />                                                                
-                            </Route>
-
-                        </Switch>
+                    <Route exact path="/courseinfo">
+                        <CourseInfo />                                
                     </Route>
+                    <Route exact path="/courseoutline">
+                        <CourseOutline />                                                                
+                    </Route>
+                    <Route exact path="/assignment">
+                        <Assignment />                                
+                    </Route>
+                    <Route exact path="/assessment">
+                        <Assessment />                                                                
+                    </Route>
+                    <Route exact path="/plagiarismpolicy">
+                        <PlagiarismPolicy />                                                                
+                    </Route>
+                    <Route exact path="/FAQ">
+                        <FAQ />                                                                
+                    </Route>                        
                 </Switch>
             </Router>
         );
