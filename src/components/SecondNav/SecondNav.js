149b8cd89745551a316839e7aee7145aee9aa345
AdamF06 <qq2841864141@gmail.com>
2019-11-13 19:10:19 +1100

    ...

--- src/components/SecondNav/SecondNav.js
+++ src/components/SecondNav/SecondNav.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './SecondNav.css'
 import UserSecondNav from '../User/Nav/secondNav'
-
+import CourseSecondNav from '../Course/Nav/secondNav'
 class SecondNav extends Component {  
 
     render() {
@@ -11,6 +11,9 @@ class SecondNav extends Component {
             <div className="secondNav">
                 <div className={`userNav ${selectedSecondNav==='Users' && showSecondNav? '--active' :'--inactive'}`}>
                     <UserSecondNav/>
+                </div>
+                <div className={`courseNav ${selectedSecondNav==='Courses' && showSecondNav? '--active' :'--inactive'}`}>
+                    <CourseSecondNav/>
                 </div>
             </div>
         );
