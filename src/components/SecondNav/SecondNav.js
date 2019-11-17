8b5e4156659f496d479d5af932aaf09e83998f80
AdamF06 <qq2841864141@gmail.com>
2019-11-17 13:00:13 +1100

    FIX-changed second navbar

--- src/components/SecondNav/SecondNav.js
+++ src/components/SecondNav/SecondNav.js
@@ -3,18 +3,20 @@ import './SecondNav.css'
 import UserSecondNav from '../User/Nav/secondNav'
 import CourseSecondNav from '../Course/Nav/secondNav'
 
-class SecondNav extends Component {  
+class SecondNav extends Component {
 
     render() {
-        const {selectedSecondNav, showSecondNav} = this.props.props
-
+        const { selectedSecondNav, showSecondNav } = this.props.props
+        console.log('from second nev ====== ',selectedSecondNav,showSecondNav)
         return (
-            <div className="secondNav">
-                <div className={`userNav ${selectedSecondNav==='Users' && showSecondNav? '--active' :'--inactive'}`}>
-                    <UserSecondNav/>
-                </div>
-                <div className={`courseNav ${selectedSecondNav==='Courses' && showSecondNav? '--active' :'--inactive'}`}>
-                    <CourseSecondNav/> 
+            <div className="secondNavContainer">
+                <div className={`secondNav ${showSecondNav?'secondNav--active':''}`}>
+                    <div className={`userNav ${selectedSecondNav === 'Users' && showSecondNav ? '--active' : '--inactive'}`}>
+                        <UserSecondNav />
+                    </div>
+                    <div className={`courseNav ${selectedSecondNav === 'Courses' && showSecondNav ? '--active' : '--inactive'}`}>
+                        <CourseSecondNav />
+                    </div>
                 </div>
             </div>
         );
