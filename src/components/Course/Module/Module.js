616557180c0148c63a058bdc74f573b2616be391
AdamF06 <qq2841864141@gmail.com>
2019-11-21 01:15:53 +1100

    Module created

---
+++ src/components/Course/Module/Module.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './Module.css'
+import ModuleItems from '../ModuleItems/ModuleItems'
+
+const dummyModules = [
+    {
+        name:'HTML',
+        date:'week_1'
+    },
+    {
+        name:'React',
+        date:'week_2'
+    },
+    {
+        name:'Mongo DB',
+        date:'week_3'
+    },
+    {
+        name:'Project',
+        date:'week_4'
+    },
+]
+class Module extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className="moduleContainer">
+                <div className="module">
+                    {
+                        dummyModules.map((item)=> <ModuleItems {...item} />)
+                    }
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default Module;
