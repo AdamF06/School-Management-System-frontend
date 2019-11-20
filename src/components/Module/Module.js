50b2a42e07e7a9fd98b2507175996a48e1f6d00c
AdamF06 <qq2841864141@gmail.com>
2019-11-21 00:23:36 +1100

    Create module page

---
+++ src/components/Module/Module.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class Module extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className="module">
+                
+            </div>
+         );
+    }
+}
+ 
+export default Module;
