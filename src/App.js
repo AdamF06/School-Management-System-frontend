05691f671cdec48d95c12e0ed41f6a562dcb45ea
AdamF06 <qq2841864141@gmail.com>
2019-12-06 22:04:16 +1100

    fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import {  Router, Switch, Route } from 'react-router-dom'
 import { connect } from 'react-redux';
 import './App.css'
 
