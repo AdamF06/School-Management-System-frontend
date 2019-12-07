2509b4f95162eca036020159130df40231ba6eac
AdamF06 <qq2841864141@gmail.com>
2019-12-07 20:01:14 +1100

    skeleton of scrolling done

--- src/components/Emrollment/Enrollment.js
+++ src/components/Emrollment/Enrollment.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './Enrollment.css'
 import EnrollItems from './EnrollItems'
 import DropItems from './DropItems'
-
+import CourseCard from './CourseCard'
 import {
     Search,
     Category,
@@ -25,48 +25,48 @@ const dummyDropList = [
         name: "Software Engineering"
     },
 ]
-const dummyCourse=[
+const dummyCourse = [
     {
         name: "Some Course a",
-        starPt:'5',
-        startDate:"Start at dd/mm/yy"
+        starPt: '5',
+        startDate: "Start at dd/mm/yy"
     },
     {
         name: "Some Course b",
-        starPt:'4',
-        startDate:"Start at dd/mm/yy"
+        starPt: '4',
+        startDate: "Start at dd/mm/yy"
     },
     {
         name: "Some Course c",
-        starPt:'3',
-        startDate:"Start at dd/mm/yy"
+        starPt: '3',
+        startDate: "Start at dd/mm/yy"
     },
     {
         name: "Some Course d",
-        starPt:'2',
-        startDate:"Start at dd/mm/yy"
+        starPt: '2',
+        startDate: "Start at dd/mm/yy"
     },
     {
         name: "Some Course e",
-        starPt:'5',
-        startDate:"Start at dd/mm/yy"
+        starPt: '5',
+        startDate: "Start at dd/mm/yy"
     },
     {
         name: "Some Course f",
-        starPt:'4',
-        startDate:"Start at dd/mm/yy"
+        starPt: '4',
+        startDate: "Start at dd/mm/yy"
     },
     {
         name: "Some Course g",
-        starPt:'3',
-        startDate:"Start at dd/mm/yy"
+        starPt: '3',
+        startDate: "Start at dd/mm/yy"
     },
     {
         name: "Some Course  h",
-        starPt:'2',
-        startDate:"Start at dd/mm/yy"
+        starPt: '2',
+        startDate: "Start at dd/mm/yy"
     },
-     
+
 ]
 class Enrollment extends Component {
     state = {
@@ -83,8 +83,8 @@ class Enrollment extends Component {
                     <div className="body__topbar">
                         <img src={require('./JRIcon.png')} alt="icon img"></img>
                         <div className='body__topbar__category'
-                        onMouseOver={()=>this.setState({dropList:true})}
-                        onMouseLeave={()=>this.setState({dropList:false})}
+                            onMouseOver={() => this.setState({ dropList: true })}
+                            onMouseLeave={() => this.setState({ dropList: false })}
                         >
                             {Category}
                             <h4>Categories</h4>
@@ -95,19 +95,30 @@ class Enrollment extends Component {
                         </from>
                     </div>
                     <div className={`dropList ${dropList ? '' : "--hide"}`}
-                    onMouseOver={()=>this.setState({dropList:true})}
-                    onMouseLeave={()=>this.setState({dropList:false})}
+                        onMouseOver={() => this.setState({ dropList: true })}
+                        onMouseLeave={() => this.setState({ dropList: false })}
                     >
                         {
                             dummyDropList.map(
-                                (item,index)=><DropItems {...item} />
+                                (item, index) => <DropItems {...item} key={index} />
                             )
                         }
                     </div>
+
+                    <div className="scrolling">
+                        <div className='scrolling__list'>
+                            <div className = "card"> 1 </div>
+                            <div className = "card"> 2 </div>
+                            <div className = "card"> 3 </div>
+                            <div className = "card"> 4 </div>
+                            <div className = "card"> 5 </div>
+                        </div>
+                    </div>
+
                     <div className="body__content">
                         {
                             dummyCourse.map(
-                                (item,index)=><EnrollItems {...item}/>
+                                (item, index) => <EnrollItems {...item} key={index} />
                             )
                         }
                     </div>
