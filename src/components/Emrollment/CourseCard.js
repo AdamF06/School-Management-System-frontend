b9f5f06e5f38ff958ff506f0e61531d7d7730858
AdamF06 <qq2841864141@gmail.com>
2019-12-07 20:01:14 +1100

    skeleton of scrolling done

---
+++ src/components/Emrollment/CourseCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const CourseCard = () => {
+    return ( 
+        <div className = "card">
+            <h3>Find more</h3>
+            <h3>Enroll in</h3>
+        </div>
+     );
+}
+ 
+export default CourseCard;
