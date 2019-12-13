e9f1f0a5618c37b55ac74a78799f4e27b2c9c597
AdamF06 <qq2841864141@gmail.com>
2019-12-13 16:20:21 +1100

    changed some profile css

--- src/components/User/Profile/profile.js
+++ src/components/User/Profile/profile.js
@@ -1,71 +1,81 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './Profile.css';
 import Items from '../Common/Items'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPortrait, faEdit, faEnvelope, faIdCard, faPhoneSquare, faSchool, faIdBadge, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
+import { faPortrait, faEdit, faPhoneSquare, faSchool, faIdBadge, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
+
 const Port = <FontAwesomeIcon icon={faPortrait} size="10x" color="grey" />
 const Pencil = <FontAwesomeIcon icon={faEdit} size="1x" color="white" />
 const Port_s = <FontAwesomeIcon icon={faPortrait} size="1x" color="grey" />
-const Email = <FontAwesomeIcon icon={faEnvelope} size="1x" color="grey" />
-const Age = <FontAwesomeIcon icon={faIdCard} size="1x" color="grey" />
 const Mobile = <FontAwesomeIcon icon={faPhoneSquare} size="1x" color="grey" />
 const School = <FontAwesomeIcon icon={faSchool} size="1x" color="grey" />
 const Title = <FontAwesomeIcon icon={faIdBadge} size="1x" color="grey" />
 const Address = <FontAwesomeIcon icon={faMapMarkedAlt} size="1x" color="grey" />
 
-const leftItems = [
-  {
-    icon: Port_s,
-    labelName: "First Name"
-  },
-  {
-    icon: Port_s,
-    labelName: "Last Name"
-  },
-  {
-    icon: Mobile,
-    labelName: "Mobile"
-  },
-  {
-    icon: School,
-    labelName: "School"
-  },
-]
-const rightItems = [
-  {
-    icon: Title,
-    labelName: "Title"
-  },
-  {
-    icon: Age,
-    labelName: "Age"
-  },
-  {
-    icon: Email,
-    labelName: "Second Email"
-  },
-]
-
 class Profile extends Component {
   state = {
-    savebuttonActive:false
+    savebuttonActive: false
   }
 
-  toggle=(e)=>{
-    e.preventDefault(); 
+  toggle = (e) => {
+    e.preventDefault();
     const current = this.state.savebuttonActive
     this.setState({
-      savebuttonActive:current?false:true
+      savebuttonActive: current ? false : true
     })
   }
 
-  componentDidMount(){}
   render() {
-    console.log(window.localStorage)
-    const {student_ID} = window.localStorage.userInfo
-    return (
-      <div className='profile'>
+    const {
+      first_name,
+      last_name,
+      student_ID,
+      address_1,
+      address_2,
+      avatar,
+      title,
+      course,
+      school,
+      mobile_number
+    } = this.props.info
 
+    const infoItems = [
+      {
+        icon: Port_s,
+        labelName: "First Name",
+        placeHolder: first_name
+
+      },
+      {
+        icon: Port_s,
+        labelName: "Last Name",
+        placeHolder: last_name
+
+      },
+      {
+        icon: Mobile,
+        labelName: "Mobile",
+        placeHolder: mobile_number
+
+      },
+      {
+        icon: School,
+        labelName: "School",
+        placeHolder: school
+      },
+      {
+        icon: Title,
+        labelName: "Title",
+        placeHolder: title
+      },
+    ]
+
+    let courseName = course.map((item) => item.course_name)
+    let finace = course.map((item) => item.paied).reduce((accumulator, currentValue) => accumulator + currentValue)
+    return (
+
+      <div className='profile'>
         <div className="profile__avatarContainer">
           <div className='avatar'> {Port} </div>
           <button className="edit"> <h6>{Pencil}</h6> <h6> Edit Avatar</h6> </button>
@@ -80,25 +90,17 @@ class Profile extends Component {
 
             <div className="main__body">
               <div className="main__body__left">
-                {leftItems.map(
-                  (item, index) => <Items {...item} key = {index}/>
+                {infoItems.map(
+                  (item, index) => <Items {...item} key={index} />
                 )}
               </div>
-
-              <div className="main__body__right">
-
-                {rightItems.map(
-                  (item, index) => <Items {...item} key = {index} />
-                )}
-              </div>
-
             </div>
             <div className="main__body__item">
               <div className="item__label">
                 <h2> {Address} </h2>
                 <h2>First Address</h2>
               </div>
-              <input></input>
+              <input placeholder={address_1}></input>
             </div>
 
             <div className="main__body__item">
@@ -106,29 +108,42 @@ class Profile extends Component {
                 <h2> {Address} </h2>
                 <h2>Second Address</h2>
               </div>
-              <input></input>
+              <input placeholder={address_2}></input>
             </div>
 
             <div className="main__body__setButton">
               <button className='editbutton' onClick={this.toggle}>Edit</button>
-              <button className={`savebutton ${this.state.savebuttonActive? '':'--inactive'}`}
-              disabled={!this.state.savebuttonActive}
-              onClick={this.toggle}
+              <button className={`savebutton ${this.state.savebuttonActive ? '' : '--inactive'}`}
+                disabled={!this.state.savebuttonActive}
+                onClick={this.toggle}
               >Save</button>
             </div>
           </div>
         </div>
 
         <div className="profile__courseContainer">
-          <h2>Student ID:</h2>
-          <h2>You have enrolled in:</h2>
-          <h2>Your finance:</h2>
+          <h2>Student ID: {student_ID}</h2>
+          <h2>You have enrolled in: </h2>
+          <ul>
+            {
+              courseName.map((item, index) =>
+                <li key={index}> {item} </li>
+              )
+            }
+          </ul>
+          <h2>Your finance: <span>${finace}</span></h2>
+          <h2>Need to be paied</h2>
           <button>Change Password</button>
         </div>
       </div>
     );
   }
 }
-
-export default Profile;
-
+function mapStateToProps(state) {
+  const { student } = state;
+  return {
+    info: student.info,
+    _id: student._id
+  };
+}
+export default connect(mapStateToProps, {})(Profile);
