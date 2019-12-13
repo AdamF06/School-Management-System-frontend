54b60cb02518c990cf6afed5d13e7191c2b46704
AdamF06 <qq2841864141@gmail.com>
2019-12-13 12:38:07 +1100

    removed old fetch course action, instant get student

--- src/apis/students.js
+++ src/apis/students.js
@@ -1,19 +1,4 @@
 import Axios from "axios"
-
-const fetchCourseApi = async () => {
-    const { token, userInfo } = window.localStorage
-    const tokenHeader = {
-        headers: {
-            Authorization: "Bearer " + token
-        }
-    }
-    const _id = JSON.parse(userInfo)._id
-    const url = 'http://127.0.0.1:8080/students/fetchCourse/' + _id;
-    
-    // return Axios.get(url, tokenHeader)  
-    return Axios.get(url, tokenHeader) 
-}
-
 const getStudentApi = async () =>{
     const { token, userInfo } = window.localStorage
     const tokenHeader = {
@@ -27,6 +12,5 @@ const getStudentApi = async () =>{
 }
 
 export {
-    fetchCourseApi,
     getStudentApi
 }
--- src/reducers/student.js
+++ src/reducers/student.js
@@ -1,26 +1,16 @@
 export const initialState = {
     info:{},
     course: [],
-    testCourse:[],
     err: ''
 };
 
 const student = (state = initialState, action) => {
     switch (action.type) {
-        case 'FETCH_SUCCEEDED':
-            return {
-                ...state,
-                course: action.data.data,
-            };
-        case 'FETCH_FAILED':
-            return {
-                ...state,
-            };
         case 'FETCH_STUDENT_SUCCEEDED':
             return {
                 ...state,
                 info: action.data.data,
-                testCourse:action.data.data.course
+                course:action.data.data.course
             };
         case 'FETCH_STUDENT_FAILED':
             return {
