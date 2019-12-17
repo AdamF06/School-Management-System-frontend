b0b35f5292aadbd254bb3a8e9641262ef0b7475d
AdamF06 <qq2841864141@gmail.com>
2019-12-17 12:31:56 +1100

    link fixed

---
+++ src/components/Course/Assignment/AssignmentDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class AssignmentDetail extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div className="assignment-detail">
+                this is ass page
+            </div>
+         );
+    }
+}
+ 
+export default AssignmentDetail;
