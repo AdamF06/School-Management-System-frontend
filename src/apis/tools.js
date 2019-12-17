6c511bc1487f71098ce57bdfa75078807cbbf53f
AdamF06 <qq2841864141@gmail.com>
2019-12-17 18:16:59 +1100

    trying update assignment

--- src/apis/tools.js
+++ src/apis/tools.js
@@ -3,6 +3,11 @@ const uploadAvatar = async (data) => {
     const url = 'http://127.0.0.1:8080/upload';
     return Axios.post(url,data)
 }
+const uploadFiles = async (data) => {
+    const url = 'http://127.0.0.1:8080/upload/file';
+    return Axios.post(url,data)
+}
 export {
-    uploadAvatar
+    uploadAvatar,
+    uploadFiles
 }
