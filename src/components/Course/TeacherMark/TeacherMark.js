605e911b9363d3e960f3c1ad6e64b57dfc837f93
AdamF06 <qq2841864141@gmail.com>
2019-12-20 19:15:38 +1100

    mark bulit, next download assignment

--- src/components/Course/TeacherMark/TeacherMark.js
+++ src/components/Course/TeacherMark/TeacherMark.js
@@ -1,27 +1,89 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Axios from 'axios'
 import './TeacherMark.css'
 import { } from '../../../actions'
+
 class TeacherMark extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            studentArray: []
         }
     }
-
+    //res.data[0].assignment.key(.name._id)
+    //res.data[0].project
 
     componentDidMount() {
-        const { student } = this.props
+        const { student, course } = this.props
+        const STUDENTS = 'http://127.0.0.1:8080/students/ID/'
 
         if (student) {
-            console.log(student)
+            student.forEach((element, index) => {
+                Axios.get(STUDENTS + element.student_ID)
+                    .then(res => {
+                        console.log(res)
+                        console.log(student)
+                        let student_data = {
+                            student_ID: student[index].student_ID,
+                            student_name: student[index].first_name + " " + student[index].last_name,
+                            assignment: res.data[0].assignment,
+                            project: res.data[0].project
+                        }
+                        console.log(student_data)
+                        this.setState({
+                            studentArray: this.state.studentArray.concat(student_data)
+                        })
+                    })
+                    .catch(err => console.log(err))
+            });
         }
     }
     render() {
-        const { student } = this.props
+        const { student, course } = this.props
+        const { studentArray } = this.state
         return (
             <div className="teacher-mark">
-                {
+                <div className="assignment">
+                    {
+                        course.assignment.map((item, index) =>
+                            <div
+                                key={index}
+                                className="assignment_container"
+                            >
+                                <h2 className="title">{item.assignment_name}</h2>
+                                {
+                                    studentArray.map((item, index2) =>
+                                        <div className="student" key={index2}>
+                                            <div className="left">
+                                                <h3> {item.student_ID}</h3>
+                                                <h3> {item.student_name}</h3>
+                                                <h3 className="key">{item.assignment[index].key}</h3>
+                                            </div>
+
+                                            <div className="right">
+                                                <h3 className="mark">{item.assignment[index].mark}Pt</h3>
+                                                <input placeholder="assign marks"></input>
+                                                <button>Update</button>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            </div>)
+                    }
+                </div>
+                <div className="project">
+                    {
+                        course.project.map((item, index) =>
+                            <div
+                                key={index}
+                                className="assignment_container"
+                            >
+                                <h2 className="title">{item.assignment_name}</h2>
+                            </div>)
+                    }
+                </div>
+                {/* {
                     student.map((item, index) =>
                         <div className="student-list" key={index}>
                             <div className="left">
@@ -36,7 +98,7 @@ class TeacherMark extends Component {
                             </div>
                         </div>
                     )
-                }
+                } */}
             </div>
         );
     }
@@ -45,7 +107,8 @@ class TeacherMark extends Component {
 function mapStateToProps(state) {
     const { course } = state;
     return {
-        student: course.data.student
+        student: course.data.student,
+        course: course.data
     };
 }
 export default connect(mapStateToProps, {})(TeacherMark)
